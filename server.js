254ec245d5a351a7568a8fcca948221d9fe7b1f9
Benicio Daniel Barreto <beni@MacBook-Pro-de-Benicio.local>
2020-02-23 11:05:41 -0300

    Add database connection

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const image = require('./controllers/image');
 const db = knex ({
   client: 'pg',
   connection: {
-    host : 'postgresql-perpendicular-73947',
-    user : 'beni',
-    password : '',
-    database : 'smartbrain'
+    host : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
